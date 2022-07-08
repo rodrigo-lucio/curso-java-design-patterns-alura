f0c1d5eef29b5018f62621966435644a6cedeab7
Rodrigo Lúcio <luciodigo@gmail.com>
2022-07-07 23:07:00 -0300

    Estudo alternativo do padrao Command

---
+++ src/main/java/br/com/lucio/alternativo/command/Loja.java
@@ -0,0 +1,15 @@
+package br.com.lucio.alternativo.command;
+
+public class Loja {
+	protected String nomeDaLoja;
+
+	public Loja(String nome) {
+		nomeDaLoja = nome;
+	}
+
+	public void executarCompra(double valor, PagamentoCommand formaDePagamento) {
+		Compra compra = new Compra(nomeDaLoja);
+		compra.setValor(valor);
+		formaDePagamento.processarCompra(compra);
+	}
+}
---
+++ src/main/java/br/com/lucio/alternativo/command/PagamentoCommand.java
@@ -0,0 +1,5 @@
+package br.com.lucio.alternativo.command;
+
+public interface PagamentoCommand {
+	void processarCompra(Compra compra);
+}
